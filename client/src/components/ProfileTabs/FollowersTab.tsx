dc50cf8802859ebc78222df44ed2b817cb6b332a
James Strande <strandejr@gmail.com>
2022-06-23 22:46:46 -0400

    added comments time and date

--- client/src/components/ProfileTabs/FollowersTab.tsx
+++ client/src/components/ProfileTabs/FollowersTab.tsx
@@ -3,7 +3,7 @@ import { useQuery } from '@apollo/client';
 import { QUERY_USER } from '../../utils/queries';
 import { useParams } from 'react-router-dom';
 import FollowUserButton from '../../components/buttons/FollowUnFollow';
-import { Card } from 'react-bootstrap'
+import { Card, Col } from 'react-bootstrap'
 
 const FollowersTab = () => {
     const { userId }: any = useParams();
@@ -16,13 +16,15 @@ const FollowersTab = () => {
     const userFollowers = user?.followers?.map((follower: any) => {
         console.log(follower)
         return (
-            <Card className='justify-content-center'>
-                <Card.Header>{follower?.username}</Card.Header>
-                <Card.Body>
-                    <Card.Subtitle><p>{follower.email}</p></Card.Subtitle>
-                </Card.Body>
-                <Card.Footer><FollowUserButton _id={follower?._id} /></Card.Footer>
-            </Card>
+            <Col>
+                <Card className='justify-content-center'>
+                    <Card.Header>{follower?.username}</Card.Header>
+                    <Card.Body>
+                        <Card.Subtitle><p>{follower.email}</p></Card.Subtitle>
+                    </Card.Body>
+                    <Card.Footer><FollowUserButton _id={follower?._id} /></Card.Footer>
+                </Card>
+            </Col>
         )
     })
 
--- client/src/components/ProfileTabs/Posts.tsx
+++ client/src/components/ProfileTabs/Posts.tsx
@@ -16,11 +16,13 @@ const UsersPostsTab = () => {
         console.log(post)
         const PostComments = post?.comments.map((comment: any) => {
             console.log('these are user comments', comment)
+            const CommentDate = new Date(comment?.createdAt)
             return (
-                <Card className='justify-content-center mb-4' key={comment?._id}>
-                    <Card.Header className='bg-primary'>{comment?.author}</Card.Header>
+                <Card className='mb-4' key={comment?._id}>
+                    <Card.Header className='bg-danger justify-content-center text-white'>{comment?.author}</Card.Header>
                     <Card.Body>
-                        <Card.Subtitle>{comment?.createdAt}</Card.Subtitle>
+                        <Card.Subtitle className='mb-3'>{CommentDate.toLocaleDateString()}</Card.Subtitle>
+                        <Card.Subtitle className='mb-3'>{CommentDate.toLocaleTimeString()}</Card.Subtitle>
                         <Card.Text>{comment?.text}</Card.Text>
                     </Card.Body>
 
@@ -29,7 +31,7 @@ const UsersPostsTab = () => {
         })
         return ( 
             <Card className='justify-content-center' key={post?._id}>
-                <Card.Header key={post?.author}>{post?.author}</Card.Header>
+                <Card.Header className='bg-primary' key={post?.author}>{post?.author}</Card.Header>
                 <Card.Body key={post?.text}>{post?.text}</Card.Body>
                 <Card.Footer>{PostComments}</Card.Footer>
             </Card>
